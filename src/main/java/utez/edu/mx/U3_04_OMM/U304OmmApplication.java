60df178d3ac9293b9ccbf3e863f8731ead043c7d
Mildret-Ventura <Mildret-Ventura>
2025-06-16 15:29:12 -0600

    [FIRST-COMMIT] | Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/U3_04_OMM/U304OmmApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.U3_04_OMM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U304OmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U304OmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utez/edu/mx/U3_04_OMM/U304OmmApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.U3_04_OMM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U304OmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
